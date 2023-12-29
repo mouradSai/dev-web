5fbff56deab80b792256ba22d79201c198461253
MokraneMassi <Mokranemassi06.com>
2023-12-29 21:50:27 +0100

    push master

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <div><h1>hello moi c'est massi </h1></div>
+       <div><h1>hello moi c'est massi </h1>
+       <h1>hello moi c'est massi </h1>
+       </div>
     </div>
   );
 }
